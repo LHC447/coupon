26d8e91f833ebdecfae81c9c9d2e1beebfebc016
him01 <glackssy@gmail.com>
2020-03-23 15:18:02 +0900

    domain, web 패키지 삭제

--- src/main/java/com/kakao/domain/CouponRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kakao.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CouponRepository extends JpaRepository<Coupon, Long>{
-	Coupon findByEmail(String email);
-}
